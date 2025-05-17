9ab3f6a4c00b61d35adad3d0b64a54ebca5056f2
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-05-17 20:27:51 +0900

    add: ans for ch2 #3

---
+++ answer/chapter2/18.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+import pandas as pd
+
+path = os.environ.get("DATA_DIR")
+filename = Path("popular-names.txt")
+
+df = pd.read_csv(path / filename, sep="\t", header=None)
+
+print(df.iloc[:, 0].value_counts())
