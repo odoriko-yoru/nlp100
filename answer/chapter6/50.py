1eaf52078a200a3044c88c0e11110614b6925f2b
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-06-01 22:01:33 +0900

    chapter6 #9

---
+++ answer/chapter6/50.py
@@ -0,0 +1,16 @@
+"""単語ベクトルの読み込みと表示."""
+
+import os
+from pathlib import Path
+
+from gensim.models import KeyedVectors
+
+data_dir = os.environ.get("DATA_DIR", "")
+data_dir = Path(data_dir)
+file = Path("GoogleNews-vectors-negative300.bin.gz")
+
+data_path = data_dir / file
+
+# Ref: https://radimrehurek.com/gensim/models/keyedvectors.html#how-to-obtain-word-vectors
+wv_from_bin = KeyedVectors.load_word2vec_format(data_path, binary=True)
+print(wv_from_bin["United_States"])
