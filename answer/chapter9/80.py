5f934fbab3ee2c9dcfdc8dfedea2d173d5db5845
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-08-05 22:05:39 +0900

    chapter9 #16

---
+++ answer/chapter9/80.py
@@ -0,0 +1,15 @@
+"""トークン化.
+
+# https://huggingface.co/answerdotai/ModernBERT-base#usage
+"""
+
+from transformers import AutoTokenizer
+
+model_id = "answerdotai/ModernBERT-base"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+text = "The movie was full of incomprehensibilities."
+
+inputs = tokenizer(text, return_tensors="pt")
+
+print(inputs)
