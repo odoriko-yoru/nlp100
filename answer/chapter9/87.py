cef1cfe55bad42b31df427a9b5b45c720c4f7fc3
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-08-21 22:22:38 +0900

    style: apply ruff #16

--- answer/chapter9/87.py
+++ answer/chapter9/87.py
@@ -47,65 +47,61 @@ def compute_accuracy(eval_pred: Tuple[np.ndarray, np.ndarray]) -> Dict[str, floa
 # tokenizerは関数のスコープ外を参照している点に注意
 # ここでpaddingは行わず、トレーニング時にバッチごとにpaddingを行う
 def tokenize_function(examples: Dict[str, Any]):
-    return tokenizer(examples["sentence"], max_length=512, truncation=True, padding=False, return_tensors=None)  # noqa
+    return tokenizer(examples["sentence"], max_length=512, truncation=True, padding=False, return_tensors=None)
 
 
-def main() -> None:
-    # Datasetの読み込み
-    train_dataset = Dataset.from_csv(f"{DATA_DIR}/SST-2/train.tsv", sep="\t")
-    dev_dataset = Dataset.from_csv(f"{DATA_DIR}/SST-2/dev.tsv", sep="\t")
+# Datasetの読み込み
+train_dataset = Dataset.from_csv(f"{DATA_DIR}/SST-2/train.tsv", sep="\t")
+dev_dataset = Dataset.from_csv(f"{DATA_DIR}/SST-2/dev.tsv", sep="\t")
 
-    # Google BERT
-    # https://huggingface.co/google-bert/bert-base-cased
-    model_id = "google-bert/bert-base-cased"
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
-
-    # tokenize
-    # ここで[CLS], [SEP] tokenも追加される
-    encoded_train_dataset = train_dataset.map(
-        tokenize_function, batched=True, batch_size=1000, num_proc=2, remove_columns=["sentence"]
-    )
-    encoded_dev_dataset = dev_dataset.map(
-        tokenize_function, batched=True, batch_size=1000, num_proc=2, remove_columns=["sentence"]
-    )
-
-    # collate関数, batchごとにPaddingを行う
-    # https://huggingface.co/docs/transformers/main_classes/data_collator#transformers.DataCollatorWithPadding
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    # トレーニング引数の設定
-    training_args = TrainingArguments(
-        output_dir="./results",
-        num_train_epochs=3,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=32,
-        learning_rate=2e-5,
-        lr_scheduler_type="linear",
-        warmup_ratio=0.1,
-        eval_strategy="epoch",
-        save_strategy="epoch",
-        load_best_model_at_end=True,
-        metric_for_best_model="accuracy",
-        fp16=True,
-        logging_dir="./logs",
-    )
-
-    trainer = Trainer(
-        model=model,
-        train_dataset=encoded_train_dataset,
-        eval_dataset=encoded_dev_dataset,
-        data_collator=data_collator,
-        args=training_args,
-        compute_metrics=compute_accuracy,
-    )
-
-    trainer.train()
-
-    # 最終評価
-    eval_results = trainer.evaluate()
-    print(f"最終評価結果: {eval_results}")
+# Google BERT
+# https://huggingface.co/google-bert/bert-base-cased
+model_id = "google-bert/bert-base-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
 
 
-if __name__ == "__main__":
-    main()
+# tokenize
+# ここで[CLS], [SEP] tokenも追加される
+encoded_train_dataset = train_dataset.map(
+    tokenize_function, batched=True, batch_size=1000, num_proc=2, remove_columns=["sentence"]
+)
+encoded_dev_dataset = dev_dataset.map(
+    tokenize_function, batched=True, batch_size=1000, num_proc=2, remove_columns=["sentence"]
+)
+
+# collate関数, batchごとにPaddingを行う
+# https://huggingface.co/docs/transformers/main_classes/data_collator#transformers.DataCollatorWithPadding
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# トレーニング引数の設定
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    learning_rate=2e-5,
+    lr_scheduler_type="linear",
+    warmup_ratio=0.1,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy",
+    fp16=True,
+    logging_dir="./logs",
+)
+
+trainer = Trainer(
+    model=model,
+    train_dataset=encoded_train_dataset,
+    eval_dataset=encoded_dev_dataset,
+    data_collator=data_collator,
+    args=training_args,
+    compute_metrics=compute_accuracy,
+)
+
+trainer.train()
+
+# 最終評価
+eval_results = trainer.evaluate()
+print(f"Accuracy after fine tuning: {eval_results}")
