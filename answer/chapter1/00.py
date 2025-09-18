72abbc903a9a56f33b1123ed1f734b80a779f22a
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-09-18 21:31:39 +0900

    apply ruff and add a comment #22

--- answer/chapter1/00.py
+++ answer/chapter1/00.py
@@ -1,6 +1,8 @@
+"""パタトクカシーー."""
+
 patrol_car = "パトカー"
 taxi = "タクシー"
 
-ans = [str1 + str2 for str1, str2 in zip(patrol_car, taxi)]
+ans = [str1 + str2 for str1, str2 in zip(patrol_car, taxi, strict=False)]
 
 print("".join(ans))
