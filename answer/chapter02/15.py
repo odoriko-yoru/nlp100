7010cd38541bbff25c38cc8d64084a22513453c1
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-09-27 22:50:22 +0900

    fix: add argparser #25 #31

--- answer/chapter02/15.py
+++ answer/chapter02/15.py
@@ -1,20 +1,32 @@
 """ファイルをN分割する."""
 
 import os
-import sys
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 
-path = os.environ.get("DATA_DIR")
-filename = Path("popular-names.txt")
 
-df = pd.read_csv(path / filename, sep="\t", header=None)
+def main(args) -> None:
+    path = os.environ.get("DATA_DIR")
+    filename = Path("popular-names.txt")
 
-n_split = int(sys.argv[1])
+    df = pd.read_csv(path / filename, sep="\t", header=None)
 
-idx = np.linspace(0, len(df), n_split, dtype=int)
+    n_split = args.n_split
 
-for i in range(len(idx) - 1):
-    print(df.iloc[idx[i] : idx[i + 1]].to_csv(header=None))
+    idx = np.linspace(0, len(df), n_split + 1, dtype=int)
+
+    for i in range(len(idx) - 1):
+        print("=" * 20 + f"{i + 1} splits" + "=" * 20)
+        print(df.iloc[idx[i] : idx[i + 1]].to_csv(header=None))
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--n_split", type=int, default=10)
+    args = parser.parse_args()
+
+    main(args)
