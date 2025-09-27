8bc76894ba30b2d5e097c2546cdbc16899ab0d88
odoriko-yoru <im.shu.hawking314@gmail.com>
2025-09-27 22:49:02 +0900

    add: comment #25 #31

--- answer/chapter02/13.py
+++ answer/chapter02/13.py
@@ -9,4 +9,6 @@ path = os.environ.get("DATA_DIR")
 filename = Path("popular-names.txt")
 
 df = pd.read_csv(path / filename, sep="\t", header=None)
+
+# タブ区切りのcsvからスペース区切りのcsvを出力
 print(df.head(10).to_csv(sep=" ", index=False, header=None))
